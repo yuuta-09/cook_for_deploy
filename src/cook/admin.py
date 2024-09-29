b8d34838efbf9eddc381d43d03f5345e8d81f120
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2024-09-29 11:04:45 +0900

    admin用のサイトを設定

--- src/cook/admin.py
+++ src/cook/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
 
+from .models import Recipe, Ingredient
+
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 3
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "posted_at"]
+    list_filter = ["posted_at", "user"]
+    search_fields = ["name"]
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ["name", "description", "posted_at"]
+            }
+        ),
+        (
+            "Advanced options",
+            {
+                "classes": ["collapse"],
+                "fields": ["user", "image"]
+            }
+        )
+    ]
+
+    inlines = [IngredientInline]
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ["name", "amount"]
+    list_filter = ["recipe"]
+    search_fields = ["name"]
+    readonly_fields = ("recipe")
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ["name", "amount"]
+            }
+        ),
+        (
+            "Advanced options",
+            {
+                "classes": ["collapse"],
+                "fields": ["recipe"]
+            }
+        )
+    ]
+
+
 # Register your models here.
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
--- src/cook/models.py
+++ src/cook/models.py
@@ -7,15 +7,22 @@ User = get_user_model()
 
 # Create your models here.
 class Recipe(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    posted_at = models.DateTimeField(default=timezone.now)
-    image = models.ImageField(upload_to="images", blank=True, null=True)
+    name = models.CharField(max_length=50, verbose_name="レシピ名")
+    description = models.TextField(verbose_name="レシピの詳細")
+    posted_at = models.DateTimeField(default=timezone.now, verbose_name="投稿日")
+    image = models.ImageField(
+        upload_to="images", blank=True, null=True, verbose_name="レシピ画像"
+    )
     user = models.ForeignKey(
         User,
         related_name='recipes',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name="投稿ユーザー"
     )
+
+    class Meta:
+        verbose_name = "レシピ"
+        verbose_name_plural = "レシピ一覧"
 
     def __str__(self) -> str:
         return self.name
@@ -28,13 +35,18 @@ class Recipe(models.Model):
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=200)
-    amount = models.CharField(max_length=100)
+    name = models.CharField(max_length=200, verbose_name="材料名")
+    amount = models.CharField(max_length=100, verbose_name="量")
     recipe = models.ForeignKey(
         Recipe,
         related_name='ingredients',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name="レシピ"
     )
+
+    class Meta:
+        verbose_name = "具材"
+        verbose_name_plural = "具材一覧"
 
     def __str__(self) -> str:
         return self.name
